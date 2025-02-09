af71f5aae35787258cd5f9788030d723debc7c66
Shivanshu Negi <divyanshunegi@gmail.com>
2025-02-09 22:28:56 +0530

    style: update profile picture with yellow background and curved lines

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,14 +6,24 @@ export default function Hero() {
       <div className='max-w-3xl mx-auto'>
         <div className='text-center'>
           {/* Profile Image */}
-          <div className='relative w-32 h-32 mx-auto mb-6'>
-            <Image
-              src='/images/PFP.jpg'
-              alt='Profile'
-              fill
-              className='rounded-full object-cover border-4 border-gray-50 shadow-lg'
-              priority
-            />
+          <div className='relative w-48 h-48 mx-auto mb-6'>
+            {/* Yellow circle background */}
+            <div className='absolute inset-0 bg-yellow-300 rounded-full'></div>
+            {/* Curved line decoration */}
+            <div className='absolute inset-0 rounded-full'>
+              <div className='absolute -left-4 top-1/2 w-8 h-16 border-l-4 border-white transform -translate-y-1/2 rounded-l-full'></div>
+              <div className='absolute -right-2 bottom-0 w-16 h-8 border-b-4 border-white rounded-b-full'></div>
+            </div>
+            {/* Profile image */}
+            <div className='absolute inset-4'>
+              <Image
+                src='/images/PFP.jpg'
+                alt='Profile'
+                fill
+                className='rounded-full object-cover'
+                priority
+              />
+            </div>
           </div>
 
           {/* Greeting */}
